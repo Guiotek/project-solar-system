2399010252a2719c7b6db83d3d5ef41fa01e1338
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-19 16:11:21 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import SolarSystem from './components/SolarSystem';
 class App extends React.Component {
   render() {
     return (
-    <>
-    <Header />
-    <SolarSystem />
-    <Title />
-    </>
+      <>
+        <Header />
+        <SolarSystem />
+        <Title />
+     </>
     );
   }
 }
