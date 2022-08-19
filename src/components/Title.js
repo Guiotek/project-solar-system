eda6e318425f49e0b74a67c1160756ee6b51edb8
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-19 16:07:48 -0300

    requisito 3

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const headline = <h2>Planetas</h2>;
+
+class Title extends Component {
+  render() {
+    return <title>{ headline }</title>;
+  }
+}
+
+export default Title;
