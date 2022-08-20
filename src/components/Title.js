a1986ee9e8f826d1f23148997a73c722cfb1f44d
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-08-20 17:06:20 -0300

    requisito 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends Component {
   render() {
-    return <title><h2>{ this.props.headline }</h2></title>;
+    const { headline } = this.props;
+    return <h2>{headline}</h2>;
   }
 }
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
